d5e8c732a16db87f3d50c52bea608bcdc59f7c15
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2019-11-01 23:42:25 +0200

    Add BottomNavigationView

--- app/src/main/java/com/dinodelivery/app/MainActivity.kt
+++ app/src/main/java/com/dinodelivery/app/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme_NoActionBar)
+        setTheme(R.style.AppTheme_NoActionBar_Colored)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
