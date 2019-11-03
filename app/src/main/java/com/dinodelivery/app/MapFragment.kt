62253a4fde1673efc770ff048832438d8d05784d
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2019-11-03 20:02:57 +0200

    Add Navigation Component. Add MapFragment

---
+++ app/src/main/java/com/dinodelivery/app/MapFragment.kt
@@ -0,0 +1,39 @@
+package com.dinodelivery.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    private val DOM_TORGOVLI_LAT = 49.441013265861436
+    private val DOM_TORGOVLI_LNG = 32.06598412245512
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(DOM_TORGOVLI_LAT, DOM_TORGOVLI_LNG), 15f))
+    }
+}
