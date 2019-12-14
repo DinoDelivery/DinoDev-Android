4ba159145f83d9d686c880788c4ae5d6375a474a
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2019-12-15 00:09:25 +0200

    Add order dialog

--- app/src/main/java/com/dinodelivery/app/adapters/OrderListAdapter.kt
+++ app/src/main/java/com/dinodelivery/app/adapters/OrderListAdapter.kt
@@ -8,7 +8,7 @@ import com.dinodelivery.app.R
 import com.dinodelivery.app.entities.Order
 import kotlinx.android.synthetic.main.order_item.view.*
 
-class OrderListAdapter(private val orders: List<Order>) :
+class OrderListAdapter(private val orders: List<Order>, private val onOrderClickListener: OrderClickListener) :
     RecyclerView.Adapter<OrderListAdapter.OrderViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
@@ -26,7 +26,12 @@ class OrderListAdapter(private val orders: List<Order>) :
     inner class OrderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun onBind(order: Order) {
             itemView.txtOrder.text = itemView.context.getString(R.string.order_number, order.id)
+            itemView.setOnClickListener { onOrderClickListener.onOrderClick(order) }
         }
+    }
+
+    interface OrderClickListener {
+        fun onOrderClick(order: Order)
     }
 
 }
--- app/src/main/java/com/dinodelivery/app/fragments/OrderFragment.kt
+++ app/src/main/java/com/dinodelivery/app/fragments/OrderFragment.kt
@@ -2,10 +2,15 @@ package com.dinodelivery.app.fragments
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,6 +20,8 @@ import com.dinodelivery.app.adapters.OrderListAdapter
 import com.dinodelivery.app.entities.Order
 import com.dinodelivery.app.viewmodels.OrderViewModel
 import kotlinx.android.synthetic.main.fragment_order.*
+import kotlinx.android.synthetic.main.order_dialog.view.*
+import kotlinx.android.synthetic.main.rate_delivery_dialog.view.*
 
 
 class OrderFragment : Fragment() {
@@ -58,7 +65,71 @@ class OrderFragment : Fragment() {
     }
 
     private fun setOrderList(orders: List<Order>) {
-        orderRecyclerView.adapter = OrderListAdapter(orders)
+        orderRecyclerView.adapter =
+            OrderListAdapter(orders, object : OrderListAdapter.OrderClickListener {
+                override fun onOrderClick(order: Order) {
+                    showOrderDetails(order)
+                }
+            })
         orderRecyclerView.addItemDecoration(OrderDividerDecoration())
+    }
+
+    private fun showOrderDetails(order: Order) {
+        val dialogView = layoutInflater.inflate(R.layout.order_dialog, null)
+        val alertDialog = AlertDialog.Builder(requireContext()).setView(dialogView).create()
+        alertDialog.window?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.dialog_background
+            )
+        )
+        with(dialogView) {
+            txtOrderItemCount.text = getString(R.string.order_item_count, order.itemCount)
+            txtOrderAddress.text = getString(R.string.order_delivery_address, order.deliveryAddress)
+            txtOrderTime.text = getString(R.string.order_delivery_time, order.deliveryTime)
+            txtOrderPrice.text = getString(R.string.order_price, order.price.round())
+            btnNotDelivered.setOnClickListener { alertDialog.dismiss() }
+            btnDelivered.setOnClickListener {
+                showRateDialog()
+                alertDialog.dismiss()
+            }
+        }
+        alertDialog.show()
+    }
+
+    private fun showRateDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.rate_delivery_dialog, null)
+        val alertDialog = AlertDialog.Builder(requireContext()).setView(dialogView).create()
+        alertDialog.window?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.custom_dialog_background
+            )
+        )
+        with(dialogView) {
+            ratingSpinner.adapter =
+                ArrayAdapter<Int>(context, R.layout.rating_spinner_item, arrayOf(1, 2, 3, 4, 5))
+            ratingSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Log.d(TAG, "${parent?.getItemAtPosition(position)}")
+                }
+
+            }
+            btnRateDelivery.setOnClickListener { alertDialog.dismiss() }
+        }
+        alertDialog.show()
+    }
+
+    private fun Double.round(decimals: Int = 2): String = "%.${decimals}f".format(this)
+
+    companion object {
+        private val TAG = OrderFragment::class.java.simpleName
     }
 }
