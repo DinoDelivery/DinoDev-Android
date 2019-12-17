421921bb1f3a5c3e9642b284e43f5c2069ece45d
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2019-12-18 01:31:03 +0200

    Show thank you dialog after order creation

--- app/src/main/java/com/dinodelivery/app/fragments/CreateOrderFragment.kt
+++ app/src/main/java/com/dinodelivery/app/fragments/CreateOrderFragment.kt
@@ -1,6 +1,7 @@
 package com.dinodelivery.app.fragments
 
 
+import android.annotation.SuppressLint
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.util.Log
@@ -9,10 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dinodelivery.app.MainActivity
 import com.dinodelivery.app.R
 import com.dinodelivery.app.utils.UserCacheUtils
+import kotlinx.android.synthetic.main.cart_order_dialog.view.*
 import kotlinx.android.synthetic.main.fragment_cart.btnMap
 import kotlinx.android.synthetic.main.fragment_cart.txtOrderPrice
 import kotlinx.android.synthetic.main.fragment_create_order.*
@@ -68,21 +72,42 @@ class CreateOrderFragment : Fragment() {
     }
 
     private fun initListeners() {
-        btnMap.setOnClickListener {
-            (requireActivity() as MainActivity).clearFragments()
-            (requireActivity() as MainActivity).navigateToFragment(MapFragment())
-        }
+        btnMap.setOnClickListener { goToMapFragment() }
         fTime.setOnClickListener { setTime() }
+        btnMakeOrder.setOnClickListener { showThankDialog() }
     }
 
+    @SuppressLint("SimpleDateFormat")
     private fun setTime() {
         val cal = Calendar.getInstance()
-        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
             cal.set(Calendar.HOUR_OF_DAY, hour)
             cal.set(Calendar.MINUTE, minute)
             fTime.setText(SimpleDateFormat("HH:mm").format(cal.time))
         }
         TimePickerDialog(requireContext(), timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+    }
+
+    private fun showThankDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.cart_order_dialog, null)
+        val alertDialog = AlertDialog.Builder(requireContext()).setView(dialogView).create()
+        alertDialog.window?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.custom_dialog_background
+            )
+        )
+        dialogView.btnGoToMain.setOnClickListener {
+            goToMapFragment()
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
+
+    }
+
+    private fun goToMapFragment() {
+        (requireActivity() as MainActivity).clearFragments()
+        (requireActivity() as MainActivity).navigateToFragment(MapFragment())
     }
 
     companion object {
